7e8a74d1b2bd26460b397f27c764aa5e8d3872b7
TheBrokenRail <connor24nolan@live.com>
2020-11-11 10:22:14 -0500

    Comment Dockerfile

--- mods/src/time.c
+++ mods/src/time.c
@@ -4,7 +4,7 @@
 // Replace gettimeofday() With clock_gettime()
 int gettimeofday(struct timeval *tv, __attribute__((unused)) void *tz) {
     struct timespec tp;
-    int ret = clock_gettime(CLOCK_MONOTONIC, &tp);
+    int ret = clock_gettime(CLOCK_MONOTONIC_COARSE, &tp);
     tv->tv_sec = tp.tv_sec;
     tv->tv_usec = tp.tv_nsec * 0.001;
     return ret;
