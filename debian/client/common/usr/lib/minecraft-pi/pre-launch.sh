7b89f7604f9c4a5953618b4161ab5d2cec9d4996
TheBrokenRail <connor24nolan@live.com>
2021-01-28 17:04:54 -0500

    Simplify Startup Scripts

--- debian/client/common/usr/lib/minecraft-pi/pre-launch.sh
+++ debian/client/common/usr/lib/minecraft-pi/pre-launch.sh
@@ -7,23 +7,21 @@ rm -rf /tmp/minecraft-pi
 mkdir -p /tmp/minecraft-pi
 
 # Start Logging
-LOG="$(mktemp -u)"
-mkfifo "${LOG}"
-tee /tmp/minecraft-pi/main.log < "${LOG}" &
-TEE_PID=$!
+touch /tmp/minecraft-pi/main.log
+tail -f /tmp/minecraft-pi/main.log &
+TAIL_PID=$!
 
 # Run
 set +e
-/usr/lib/minecraft-pi/run.sh > "${LOG}" 2>&1
+/usr/lib/minecraft-pi/run.sh > /tmp/minecraft-pi/main.log 2>&1
 RET=$?
 set -e
 
 # Kill Logging
-kill ${TEE_PID}
-rm "${LOG}"
+kill ${TAIL_PID}
 
 # Handle Crash
-if [ $RET -ne 0 ]; then
-    zenity --class "${ZENITY_CLASS}" --error --no-wrap --text 'Minecraft: Pi Edition has crashed!\nLogs are located in /tmp/minecraft-pi.'
-    exit $RET
+if [ ${RET} -ne 0 ]; then
+    zenity --class "${ZENITY_CLASS}" --error --no-wrap --text 'Minecraft: Pi Edition has crashed!\nLogs are located in /tmp/minecraft-pi.\n\nExit Code: '${RET}
+    exit ${RET}
 fi
