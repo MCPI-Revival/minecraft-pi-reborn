6ab07f04db48380a6354a142888a03a3d373b897
TheBrokenRail <connor24nolan@live.com>
2021-02-10 20:03:19 -0500

    Improve Image

--- launcher/src/run.sh
+++ launcher/src/run.sh
@@ -8,24 +8,27 @@ if [ ! "$(id -u)" = '0' ]; then
     exit 1
 fi
 
-# Create User Groups
-if [ -z "${USER_GID+x}" ]; then
-    USER_GID='1000'
-fi
-groupadd --force --gid "${USER_GID}" user
+# Check
+if ! id user > /dev/null 2>&1; then
+    # Create User Groups
+    if [ -z "${USER_GID+x}" ]; then
+        USER_GID='1000'
+    fi
+    groupadd --force --gid "${USER_GID}" user
 
-# Create User
-if [ -z "${USER_UID+x}" ]; then
-    USER_UID='1000'
-fi
-useradd --shell /bin/sh --home-dir /home --no-create-home --uid "${USER_UID}" --gid "${USER_GID}" user
+    # Create User
+    if [ -z "${USER_UID+x}" ]; then
+        USER_UID='1000'
+    fi
+    useradd --shell /bin/sh --home-dir /home --no-create-home --uid "${USER_UID}" --gid "${USER_GID}" user
 
-# Add Other Groups
-if [ ! -z "${USER_OTHER_GIDS+x}" ]; then
-    for gid in ${USER_OTHER_GIDS}; do
-        groupadd --force --gid "${gid}" "group-${gid}"
-        usermod -aG "${gid}" user
-    done
+    # Add Other Groups
+    if [ ! -z "${USER_OTHER_GIDS+x}" ]; then
+        for gid in ${USER_OTHER_GIDS}; do
+            groupadd --force --gid "${gid}" "group-${gid}"
+            usermod -aG "${gid}" user
+        done
+    fi
 fi
 
 # Start
