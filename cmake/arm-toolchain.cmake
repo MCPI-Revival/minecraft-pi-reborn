1daede7dba20c702e8d3c1700c3539977cdb004a
TheBrokenRail <connor24nolan@live.com>
2021-10-22 22:39:26 -0400

    Fix ARM Toolchain On Some Devices

--- cmake/arm-toolchain.cmake
+++ cmake/arm-toolchain.cmake
@@ -2,7 +2,7 @@
 if(CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "aarch64_be" OR CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "aarch64" OR CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "armv8b" OR CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "armv8l")
     # Force 32-Bit Compile
     add_compile_options("-m32")
-elseif(NOT CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "arm")
+elseif((NOT CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "arm") AND (NOT CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "armv7l"))
     # Use ARM Cross-Compiler
     include("${CMAKE_CURRENT_LIST_DIR}/base-toolchain.cmake")
     setup_toolchain("arm-linux-gnueabihf")
