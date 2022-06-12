f1ec29ec86f6ef34f30d4ef738b68f71663da467
TheBrokenRail <connor24nolan@live.com>
2022-06-12 16:23:32 -0400

    Build Tweak

--- cmake/base-toolchain.cmake
+++ cmake/base-toolchain.cmake
@@ -3,9 +3,13 @@ macro(setup_toolchain target)
     # Use ARM Cross-Compiler
     set(CMAKE_C_COMPILER "${target}-gcc")
     set(CMAKE_CXX_COMPILER "${target}-g++")
-    set(CMAKE_FIND_ROOT_PATH "/usr/${target}" "/usr/lib/${target}")
     # Extra
     set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-    # pkg-config
-    set(ENV{PKG_CONFIG_LIBDIR} "/usr/lib/${target}/pkgconfig:/usr/${target}/lib/pkgconfig:/usr/lib/pkgconfig:/usr/share/pkgconfig")
+    # Custom Search Paths
+    if(NOT DEFINED ENV{MCPI_TOOLCHAIN_USE_DEFAULT_SEARCH_PATHS})
+        # Find Root
+        set(CMAKE_FIND_ROOT_PATH "/usr/${target}" "/usr/lib/${target}")
+        # pkg-config
+        set(ENV{PKG_CONFIG_LIBDIR} "/usr/lib/${target}/pkgconfig:/usr/${target}/lib/pkgconfig:/usr/lib/pkgconfig:/usr/share/pkgconfig")
+    endif()
 endmacro()
--- docs/BUILDING.md
+++ docs/BUILDING.md
@@ -32,3 +32,6 @@
 ./scripts/setup.sh <client|server> <armhf|arm64|i686|amd64> <Custom CMake Arguments>
 ./scripts/build.sh <client|server> <armhf|arm64|i686|amd64>
 ```
+
+### Environment Variables
+- ``MCPI_TOOLCHAIN_USE_DEFAULT_SEARCH_PATHS``: Use Default CMake Search Paths Rather Than Guessing
