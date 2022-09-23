eb3c5d2e6fc28d309bd4a36836357a796dbbc5fb
TheBrokenRail <connor24nolan@live.com>
2022-09-23 17:56:44 -0400

    Remove Unused File

--- cmake/toolchain/i386-toolchain.cmake
+++
@@ -1,9 +0,0 @@
-# Warning
-message(WARNING "i686 Builds Are Unsupported, Proceed At Your Own Risk")
-# Compile For i686
-include("${CMAKE_CURRENT_LIST_DIR}/base-toolchain.cmake")
-# Use i686 Cross-Compiler
-setup_toolchain("i686-linux-gnu")
-# Details
-set(CMAKE_SYSTEM_NAME "Linux")
-set(CMAKE_SYSTEM_PROCESSOR "i686")
