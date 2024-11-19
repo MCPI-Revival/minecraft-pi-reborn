871288ee12b9163ac8d4ccc67b14de1bd138a44f
TheBrokenRail <connor24nolan@live.com>
2024-11-18 20:15:15 -0500

    Fix Example Mod (Again)

--- example-mods/chat-commands/chat-commands.cpp
+++ example-mods/chat-commands/chat-commands.cpp
@@ -17,9 +17,8 @@ HOOK(chat_handle_packet_send, void, (const Minecraft *minecraft, ChatPacket *pac
         if (out.length() > 0 && out[out.length() - 1] == '\n') {
             out[out.length() - 1] = '\0';
         }
-        char *cp437_out = to_cp437(out.c_str());
+        std::string cp437_out = to_cp437(out);
         gui->addMessage(cp437_out);
-        free(cp437_out);
     } else {
         // Call Original Method
         real_chat_handle_packet_send()(minecraft, packet);
