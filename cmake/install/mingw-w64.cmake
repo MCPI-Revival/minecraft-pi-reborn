e552e2c9d57cc0999678a948d60ad2b1a61b5547
TheBrokenRail <connor24nolan@live.com>
2026-02-28 05:08:52 -0500

    Better Selection Of MinGw Libraries

--- cmake/install/mingw-w64.cmake
+++ cmake/install/mingw-w64.cmake
@@ -8,16 +8,13 @@ execute_process(
 cmake_path(GET root PARENT_PATH root)
 
 # Install Libraries
-file(GLOB files
-    LIST_DIRECTORIES FALSE
-    "${root}/*.dll"
+install(
+    PROGRAMS
+        "${root}/libgcc_s_seh-1.dll"
+        "${root}/libstdc++-6.dll"
+        "/usr/x86_64-w64-mingw32/lib/libwinpthread-1.dll"
+    DESTINATION "${MCPI_LIB_DIR}"
 )
-foreach(file IN LISTS files)
-    install(
-        PROGRAMS "${file}"
-        DESTINATION "${MCPI_LIB_DIR}"
-    )
-endforeach()
 
 # Install Licenses
 install(
